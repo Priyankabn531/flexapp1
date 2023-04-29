0d145c2bbf1b82aa0da3f41cd55911295e680495
Priyankabn531 <132123191+Priyankabn531@users.noreply.github.com>
2023-04-29 11:26:37 +0530

    Add files via upload

---
+++ java/index1.js
@@ -0,0 +1,40 @@
+//var n=1;
+//while(n<=10){
+  //  console.log("15*"+n+"="+15*n);
+    //n++;
+//}
+
+//var n=1;
+//do{
+  // console.log("15*"+n+"="+15*n);
+    //n++;
+//}
+//while(n<=10);
+
+//var i;
+//for(i=1;i<=10;i++){
+//    console.log("15*"+i+"="+15*i);
+//}
+
+var funExp=function(a,b)
+{
+  return total=a+b;
+}
+var sum=15+20;
+var sum1=15+20;
+var sum2=15+20;
+console.log("the sum of two numbers is "+sum);
+console.log("the sum of two numbers is "+sum1);
+console.log("the sum of two numbers is "+sum2);
+
+if(sum>>=sum1&&sum>=sum2){
+  greater =sum;
+}
+else if(sum1>>=sum&&sum1>=sum2){
+  greater =sum1;
+}
+else
+{
+  greater =sum2;
+}
+console.log("the sum of two numbers is "+greater);
